c3c646c358ce664a3aa7ad51f5dece795da85204
EvgenyVarlamov <varlamov-el@yandex.ru>
2022-05-20 22:21:07 +0300

    add Homework #2 Sum of numbers between M and N

---
+++ HomeTask002/Program.cs
@@ -0,0 +1,25 @@
+﻿/* Задача 66: Задайте значения M и N. Напишите программу, 
+которая найдет сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8 -> 30
+*/
+
+int numberStart = Input("Введите значение M: ");
+int numberEnd = Input("Введите значение N: ");
+
+Console.WriteLine(OutputFunction(numberStart, numberEnd));
+
+int Input(string text)
+{
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int OutputFunction(int numStart, int numEnd)
+{
+    if (numEnd == numStart)
+    {
+        return numStart;
+    }
+    return  numEnd + OutputFunction(numStart, numEnd - 1);
+}
