e916eb3fba05b72bc5c15eabbd7e2bf92bd27df5
EvgenyVarlamov <varlamov-el@yandex.ru>
2022-05-20 22:18:36 +0300

    add Homework #1 Decrease row from N to 1

---
+++ HomeTask001/Program.cs
@@ -0,0 +1,25 @@
+﻿/* Задача 64: Задайте значение N. Напишите программу, 
+которая выведет натуральные числа в промежутке от N до 1.
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2,"
+*/
+
+int number = Input("Введите значение N: ");
+
+Console.WriteLine(OutputFunction(number));
+
+int Input(string text)
+{
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int OutputFunction(int num)
+{
+    if (num == 1)
+    {
+        return 1;
+    }
+    Console.Write(num + ", ");
+    return OutputFunction(num-1);
+}
